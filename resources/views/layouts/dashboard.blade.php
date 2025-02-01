80a47ee7c42ad2e6252b5040b215ebf5d68cf58f
Hammam GNF <hammamgonjil@gmail.com>
2025-02-01 21:16:51 +0700

    make layouts

---
+++ resources/views/layouts/dashboard.blade.php
@@ -0,0 +1,95 @@
+@extends('layouts.template')
+
+@section('page-scripts')
+@include('layouts.partials.scripts')
+@endsection
+
+@section('content')
+<div class="content vh-100">
+    <div class="row">
+        <div class="col-lg-3 col-md-6 col-sm-6">
+            <div class="card card-stats">
+                <div class="card-body ">
+                    <div class="row">
+                        <div class="col-5 col-md-4">
+                            <div class="icon-big text-center icon-warning">
+                                <i class="fa fa-users text-warning" aria-hidden="true"></i>
+                            </div>
+                        </div>
+                        <div class="col-7 col-md-8">
+                            <div class="numbers">
+                                <p class="card-category">Total Petugas</p>
+                                <p class="card-title" id="total-petugas">
+                                    Loading...
+                                <p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer ">
+                    <hr>
+                    <div class="stats">
+                        <i class="fa fa-refresh"></i>
+                        Update Now
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-6">
+            <div class="card card-stats">
+                <div class="card-body ">
+                    <div class="row">
+                        <div class="col-5 col-md-4">
+                            <div class="icon-big text-center icon-warning">
+                                <i class="bi bi-tencent-qq text-primary" aria-hidden="true"></i>
+                            </div>
+                        </div>
+                        <div class="col-7 col-md-8">
+                            <div class="numbers">
+                                <p class="card-category">Total Kategori</p>
+                                <p class="card-title" id="total-kategori">
+                                    Loading...
+                                <p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer ">
+                    <hr>
+                    <div class="stats">
+                        <i class="fa fa-calendar-o"></i>
+                        Last day
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-6">
+            <div class="card card-stats">
+                <div class="card-body ">
+                    <div class="row">
+                        <div class="col-5 col-md-4">
+                            <div class="icon-big text-center icon-warning">
+                                <i class="bi bi-bandaid text-success"></i>
+                            </div>
+                        </div>
+                        <div class="col-7 col-md-8">
+                            <div class="numbers">
+                                <p class="card-category">Total Pengaduan</p>
+                                <p class="card-title" id="total-pengaduan">Loading...
+                                <p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer ">
+                    <hr>
+                    <div class="stats">
+                        <i class="fa fa-refresh"></i>
+                        Update now
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
