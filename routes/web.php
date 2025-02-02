3d38b702e22111d4d1910b64fe83bfe5c60ba617
Hammam GNF <hammamgonjil@gmail.com>
2025-02-02 16:34:08 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/petugas', function () {
 Route::post('/petugas/store', [PetugasController::class, 'store'])->name('petugas.store');
 Route::get('/petugas/getall', [PetugasController::class, 'getall'])->name('petugas.getall');
 Route::get('/petugas/count', [PetugasController::class, 'count'])->name('petugas.count');
-Route::get('/petugas/{id}/edit', [PetugasController::class, 'edit'])->name('petugas.edit');
+Route::get('/petugas/{id_petugas}/edit', [PetugasController::class, 'edit'])->name('petugas.edit');
 Route::post('/petugas/update', [PetugasController::class, 'update'])->name('petugas.update');
 Route::delete('/petugas/delete', [PetugasController::class, 'delete'])->name('petugas.delete');
 
@@ -30,18 +30,18 @@ Route::get('/kategori', function () {
 Route::post('/kategori/store', [KategoriController::class, 'store'])->name('kategori.store');
 Route::get('/kategori/getall', [KategoriController::class, 'getall'])->name('kategori.getall');
 Route::get('/kategori/count', [KategoriController::class, 'count'])->name('kategori.count');
-Route::get('/kategori/{id}/edit', [KategoriController::class, 'edit'])->name('kategori.edit');
+Route::get('/kategori/{id_kategori}/edit', [KategoriController::class, 'edit'])->name('kategori.edit');
 Route::post('/kategori/update', [KategoriController::class, 'update'])->name('kategori.update');
 Route::delete('/kategori/delete', [KategoriController::class, 'delete'])->name('kategori.delete');
 
 Route::get('/pengaduan', function () {
-    return view('pengaduan.index');
-})->name('pengaduan.index');
+    return view('pengaduan.data.index');
+})->name('pengaduan.data.index');
 
 // Route untuk CRUD operasi pengaduan
 Route::post('/pengaduan/store', [PengaduanController::class, 'store'])->name('pengaduan.store');
 Route::get('/pengaduan/getall', [PengaduanController::class, 'getall'])->name('pengaduan.getall');
 Route::get('/pengaduan/count', [PengaduanController::class, 'count'])->name('pengaduan.count');
-Route::get('/pengaduan/{id}/edit', [PengaduanController::class, 'edit'])->name('pengaduan.edit');
+Route::get('/pengaduan/{id_pengaduan}/edit', [PengaduanController::class, 'edit'])->name('pengaduan.edit');
 Route::put('/pengaduan/update/{id}', [PengaduanController::class, 'update'])->name('pengaduan.update');
 Route::delete('/pengaduan/delete', [PengaduanController::class, 'delete'])->name('pengaduan.delete');
