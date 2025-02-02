0ce74519d92f71ad442f386e67f1f1855e4fe240
Hammam GNF <hammamgonjil@gmail.com>
2025-02-02 16:31:15 +0700

    add kategori

---
+++ resources/views/kategori/partials/table.blade.php
@@ -0,0 +1,11 @@
+<div class="table-responsive">
+    <table id="myTable" class="display">
+        <thead>
+            <tr>
+                <th class="text-center">No</th>
+                <th>Nama Kategori</th>
+                <th>Aksi</th>
+            </tr>
+        </thead>
+    </table>
+</div>
